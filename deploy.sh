e51a0dab1f7fe4e0eb4c7ca3a0b6347085c5b2b7
Sean Conrad <seanconrad123@gmail.com>
2020-10-25 21:32:12 -0400

    pushing up latest

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,12 @@ set -e # exit entire script when command exits with non-zero status
 
 npm install
 
+export DELIVER_USERNAME="test"
+export DELIVER_PASSWORD="test"
+export FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD="test"
+
+node ./updateBuildNumber.js
+
 expo publish --release-channel production --non-interactive
 
 
@@ -24,9 +30,5 @@ expo publish --release-channel production --non-interactive
 curl -o app.ipa "$(expo url:ipa --non-interactive)"
 expo build:ios --release-channel production --non-interactive --no-publish
 
-fastlane deliver --verbose --ipa "app.ipa" --skip_screenshots --skip_metadata
+fastlane deliver --verbose --ipa "app.ipa" --skip_screenshots --skip_metadata --username "seanconrad123@gmail.com" --team_id "119738131"
 #IOS END
-
-
-
-
